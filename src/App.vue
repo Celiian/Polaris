36a70a5dc0c046ecd879a3800a5b063fc475a724
Jerance <michael.jerance@gmail.com>
2023-04-18 12:30:24 +0200

    FEAT/ Refont Home page & delete useless icons vue

--- src/App.vue
+++ src/App.vue
@@ -4,29 +4,6 @@ import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-        <div class="flex justify-center items-center h-screen">
-      <h1 class="text-4xl font-bold text-black-800">Hello, world!</h1>
-    </div>
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,112 @@
-<script setup>
-import TheWelcome from "../components/TheWelcome.vue";
+<template>
+  <div class="home-page">
+    <div class="video-wrapper">
+      <video autoplay loop muted src="/src/assets/video/background-home.webm"></video>
+    </div>
+
+    <div class="main-content">
+      <h1 class="flex w-full justify-center font-bold text-center text-white">Bienvenue sur Colons de Polaris</h1>
+      <div class="container-main">
+        <div class="bg-white rounded-md shadow-md p-4 mb-4">
+          <h2 class="text-lg font-bold mb-2">Entrez votre nom</h2>
+          <form>
+            <div class="mb-4">
+              <label for="name" class="block text-gray-700 font-bold mb-2">Nom:</label>
+              <input type="text" id="name" name="name"
+                class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                placeholder="Entrez votre nom">
+            </div>
+            <button type="submit"
+              class="bg-purple-600 text-white px-4 py-2 rounded-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50">Envoyer</button>
+          </form>
+        </div>
+        <div class="bg-white rounded-md shadow-md p-4">
+          <h2 class="text-lg font-bold mb-2">Règles du jeu</h2>
+          <ul class="list-disc list-inside">
+            <li>Le jeu se joue à deux joueurs.</li>
+            <li>Chaque joueur a 21 pièces.</li>
+            <li>Les joueurs se relaient pour placer une pièce sur le plateau.</li>
+            <li>Le premier joueur à aligner 4 pièces gagne.</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+
+  data() {
+    return {
+      playerName: '',
+    }
+  },
+
+  methods: {
+    startGame() {
+
+    }
+  }
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+.home-page {
+  height: 100vh;
+}
+
+video {
+  position: fixed;
+  z-index: -1;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.sidebar {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #242424;
+  color: white;
+  padding: 2rem;
+  width: 20%;
+}
+
+.main-content {
+  padding: 2rem;
+  width: 100%;
+}
+
+.main-content h1 {
+  font-size: 4rem;
+  margin-bottom: 2rem;
+}
+
+.rules h2 {
+  font-size: 2rem;
+  margin-bottom: 1.5rem;
+}
+
+.name-form h2 {
+  font-size: 2rem;
+}
+
+label {
+  display: block;
+  font-size: 1.5rem;
+}
+
+.container-main {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  align-items: center;
+  justify-content: space-around;
+}
+</style>
